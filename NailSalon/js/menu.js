89161c942c5b0e476b4dde6fe4450ff613e9ea39
Likhaya-coder <60583056+Likhaya-coder@users.noreply.github.com>
2022-05-15 21:17:13 +0200

    Add files via upload

--- NailSalon/js/menu.js
+++ NailSalon/js/menu.js
@@ -1,9 +1,12 @@
 const toggle = document.querySelector('#toggle');
+const getContainer = document.querySelector('.nav-link .container');
+const getLinks = document.querySelector('.nav-link ul');
 let toggleNavStatus = false;
+let windowWidth = window.innerWidth;
+let caltulations = 768;
 
 let toggleClick = function() {
-    const getContainer = document.querySelector('.nav-link .container');
-    const getLinks = document.querySelector('.nav-link ul');
+    
 
     console.log(getContainer);
 
@@ -20,6 +23,9 @@ let toggleClick = function() {
     }
 }
 
-if (getContainer == getContainer.style.hidden) {
+console.log(caltulations);
+console.log(windowWidth);
+
+if (windowWidth > caltulations) {
     getLinks.style.visibility="visible";
 }
